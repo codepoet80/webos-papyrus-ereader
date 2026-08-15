397ea58d5a25284b4393cf336982e7933d847896
codepoet80 <codepoet80@github.com>
2026-08-15 13:17:23 -0400

    bump build

--- app/app/Main.js
+++ app/app/Main.js
@@ -1225,7 +1225,7 @@ enyo.kind({
 			}
 		} catch (e) {}
 
-		this.$.versionText.setContent($L("Version: ") + version + " (build v167)");
+		this.$.versionText.setContent($L("Version: ") + version + " (build v169)");
 		this.$.aboutPopup.openAtCenter();
 	},
 
